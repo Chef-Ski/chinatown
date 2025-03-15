c47055605551a715f0db4bc4137f005b0f90311d
jacob-s-codes <jacobbshaul@gmail.com>
2025-03-15 11:54:23 -0700

    updated frontend

---
+++ frontend/src/app/component/Navbar.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='w-full sticky top-0'>
+            <ul className='flex flex-row items-center justify-between px-12'>
+                <div className='flex flex-row items-center gap-x-12 text-xl'>
+                    <a href="/"><img src="/postcss.config.png" alt="home logo" className='w-36 h-auto' /></a>
+                    <li>CREATE</li>
+                </div>
+
+                <div className='flex flex-row items-center gap-x-6 text-xl'>
+                    <a href=""><li className='border border-black rounded-2xl px-8 py-2 hover:bg-gray-300'>Login</li></a>
+                    <a href=""><li className='border border-black rounded-2xl px-8 py-2 hover:bg-gray-300'>Sign Up</li></a>
+                </div>
+
+            </ul>
+
+        </div>
+    )
+}
+
+export default Navbar
